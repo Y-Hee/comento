9014a22e3408bd0efb913761b34beba0717b55c1
Y-Hee <19any19@gmail.com>
2021-11-26 01:20:24 +0900

    게시판 구현 완성

--- first/src/main/java/com/cmt/persistence/BoardDAOImpl.java
+++ first/src/main/java/com/cmt/persistence/BoardDAOImpl.java
@@ -20,35 +20,34 @@ public class BoardDAOImpl implements BoardDAO {
 	@Override
 	public void create(BoardVO vo) throws Exception {
 		session.insert(namespace+".create", vo);
-		// TODO Auto-generated method stub
+
 
 	}
 
 	@Override
 	public BoardVO read(Integer bno) throws Exception {
 		return session.selectOne(namespace + ".read", bno);
-		// TODO Auto-generated method stub
+
 		
 	}
 
 	@Override
 	public void update(BoardVO vo) throws Exception {
 		session.update(namespace+".update", vo);
-		// TODO Auto-generated method stub
+
 
 	}
 
 	@Override
 	public void delete(Integer bno) throws Exception {
 		session.delete(namespace+".delete", bno);
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public List<BoardVO> listAll() throws Exception {
 		return session.selectList(namespace + ".listAll");
-		// TODO Auto-generated method stub
+
 		
 	}
 
---
+++ first/src/main/java/com/cmt/service/BoardService.java
@@ -0,0 +1,21 @@
+package com.cmt.service;
+
+import java.util.List;
+
+import com.cmt.domain.BoardVO;
+
+public interface BoardService {
+	  public void regist(BoardVO board) throws Exception;
+
+
+	  public BoardVO read(Integer bno) throws Exception;
+
+
+	  public void modify(BoardVO board) throws Exception;
+
+
+	  public void remove(Integer bno) throws Exception;
+
+
+	  public List<BoardVO> listAll() throws Exception;
+}
